c3b60cec7c691eacc395e01fb18f02c332b763d8
topher-au <lordramza@gmail.com>
2015-10-04 01:58:01 +1000

    version 0.1

---
+++ SnakeBite/formMain.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class formMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.panelWelcome = New System.Windows.Forms.Panel()
+        Me.labelNoUninstall = New System.Windows.Forms.Label()
+        Me.labelWelcome = New System.Windows.Forms.Label()
+        Me.buttonContinue = New System.Windows.Forms.Button()
+        Me.panelSelectDatfile = New System.Windows.Forms.Panel()
+        Me.buttonBrowseMgsV = New System.Windows.Forms.Button()
+        Me.textMGSVDir = New System.Windows.Forms.TextBox()
+        Me.labelSelectMgsV = New System.Windows.Forms.Label()
+        Me.panelSelectModfile = New System.Windows.Forms.Panel()
+        Me.buttonBrowseMod = New System.Windows.Forms.Button()
+        Me.textModFile = New System.Windows.Forms.TextBox()
+        Me.labelSelectModfile = New System.Windows.Forms.Label()
+        Me.panelModInfo = New System.Windows.Forms.Panel()
+        Me.linkModSite = New System.Windows.Forms.LinkLabel()
+        Me.labelModAuthor = New System.Windows.Forms.Label()
+        Me.labelModname = New System.Windows.Forms.Label()
+        Me.buttonUninstall = New System.Windows.Forms.Button()
+        Me.panelInstall = New System.Windows.Forms.Panel()
+        Me.labelInstall = New System.Windows.Forms.Label()
+        Me.panelWelcome.SuspendLayout()
+        Me.panelSelectDatfile.SuspendLayout()
+        Me.panelSelectModfile.SuspendLayout()
+        Me.panelModInfo.SuspendLayout()
+        Me.panelInstall.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelWelcome
+        '
+        Me.panelWelcome.Controls.Add(Me.labelNoUninstall)
+        Me.panelWelcome.Controls.Add(Me.labelWelcome)
+        Me.panelWelcome.Location = New System.Drawing.Point(12, 12)
+        Me.panelWelcome.Name = "panelWelcome"
+        Me.panelWelcome.Size = New System.Drawing.Size(293, 161)
+        Me.panelWelcome.TabIndex = 0
+        '
+        'labelNoUninstall
+        '
+        Me.labelNoUninstall.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelNoUninstall.ForeColor = System.Drawing.Color.Red
+        Me.labelNoUninstall.Location = New System.Drawing.Point(17, 76)
+        Me.labelNoUninstall.Name = "labelNoUninstall"
+        Me.labelNoUninstall.Size = New System.Drawing.Size(258, 73)
+        Me.labelNoUninstall.TabIndex = 1
+        Me.labelNoUninstall.Text = "NOTE:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "SnakeBite does not include an singular" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " uninstall feature. To uninstall A" &
+    "LL of your mods, please click Uninstall and reverify the Steam data files for th" &
+    "e game."
+        '
+        'labelWelcome
+        '
+        Me.labelWelcome.Location = New System.Drawing.Point(17, 17)
+        Me.labelWelcome.Name = "labelWelcome"
+        Me.labelWelcome.Size = New System.Drawing.Size(258, 47)
+        Me.labelWelcome.TabIndex = 0
+        Me.labelWelcome.Text = "Welcome to SnakeBite MGSV Patcher, please follow the instructions below to instal" &
+    "l your mod for Metal Gear Solid V: The Phantom Pain."
+        '
+        'buttonContinue
+        '
+        Me.buttonContinue.Location = New System.Drawing.Point(230, 179)
+        Me.buttonContinue.Name = "buttonContinue"
+        Me.buttonContinue.Size = New System.Drawing.Size(75, 23)
+        Me.buttonContinue.TabIndex = 1
+        Me.buttonContinue.Text = "Continue"
+        Me.buttonContinue.UseVisualStyleBackColor = True
+        '
+        'panelSelectDatfile
+        '
+        Me.panelSelectDatfile.Controls.Add(Me.buttonBrowseMgsV)
+        Me.panelSelectDatfile.Controls.Add(Me.textMGSVDir)
+        Me.panelSelectDatfile.Controls.Add(Me.labelSelectMgsV)
+        Me.panelSelectDatfile.Location = New System.Drawing.Point(12, 12)
+        Me.panelSelectDatfile.Name = "panelSelectDatfile"
+        Me.panelSelectDatfile.Size = New System.Drawing.Size(293, 161)
+        Me.panelSelectDatfile.TabIndex = 2
+        '
+        'buttonBrowseMgsV
+        '
+        Me.buttonBrowseMgsV.Location = New System.Drawing.Point(198, 79)
+        Me.buttonBrowseMgsV.Name = "buttonBrowseMgsV"
+        Me.buttonBrowseMgsV.Size = New System.Drawing.Size(75, 23)
+        Me.buttonBrowseMgsV.TabIndex = 3
+        Me.buttonBrowseMgsV.Text = "Browse..."
+        Me.buttonBrowseMgsV.UseVisualStyleBackColor = True
+        '
+        'textMGSVDir
+        '
+        Me.textMGSVDir.Location = New System.Drawing.Point(18, 53)
+        Me.textMGSVDir.Name = "textMGSVDir"
+        Me.textMGSVDir.Size = New System.Drawing.Size(255, 20)
+        Me.textMGSVDir.TabIndex = 2
+        '
+        'labelSelectMgsV
+        '
+        Me.labelSelectMgsV.Location = New System.Drawing.Point(15, 17)
+        Me.labelSelectMgsV.Name = "labelSelectMgsV"
+        Me.labelSelectMgsV.Size = New System.Drawing.Size(258, 33)
+        Me.labelSelectMgsV.TabIndex = 1
+        Me.labelSelectMgsV.Text = "Please select your Metal Gear Solid V installation directory"
+        '
+        'panelSelectModfile
+        '
+        Me.panelSelectModfile.Controls.Add(Me.buttonBrowseMod)
+        Me.panelSelectModfile.Controls.Add(Me.textModFile)
+        Me.panelSelectModfile.Controls.Add(Me.labelSelectModfile)
+        Me.panelSelectModfile.Location = New System.Drawing.Point(12, 12)
+        Me.panelSelectModfile.Name = "panelSelectModfile"
+        Me.panelSelectModfile.Size = New System.Drawing.Size(293, 161)
+        Me.panelSelectModfile.TabIndex = 3
+        '
+        'buttonBrowseMod
+        '
+        Me.buttonBrowseMod.Location = New System.Drawing.Point(198, 79)
+        Me.buttonBrowseMod.Name = "buttonBrowseMod"
+        Me.buttonBrowseMod.Size = New System.Drawing.Size(75, 23)
+        Me.buttonBrowseMod.TabIndex = 3
+        Me.buttonBrowseMod.Text = "Browse..."
+        Me.buttonBrowseMod.UseVisualStyleBackColor = True
+        '
+        'textModFile
+        '
+        Me.textModFile.Location = New System.Drawing.Point(18, 53)
+        Me.textModFile.Name = "textModFile"
+        Me.textModFile.Size = New System.Drawing.Size(255, 20)
+        Me.textModFile.TabIndex = 2
+        '
+        'labelSelectModfile
+        '
+        Me.labelSelectModfile.Location = New System.Drawing.Point(15, 17)
+        Me.labelSelectModfile.Name = "labelSelectModfile"
+        Me.labelSelectModfile.Size = New System.Drawing.Size(258, 33)
+        Me.labelSelectModfile.TabIndex = 1
+        Me.labelSelectModfile.Text = "Please select the mod file you would like to install"
+        '
+        'panelModInfo
+        '
+        Me.panelModInfo.Controls.Add(Me.linkModSite)
+        Me.panelModInfo.Controls.Add(Me.labelModAuthor)
+        Me.panelModInfo.Controls.Add(Me.labelModname)
+        Me.panelModInfo.Location = New System.Drawing.Point(12, 12)
+        Me.panelModInfo.Name = "panelModInfo"
+        Me.panelModInfo.Size = New System.Drawing.Size(293, 161)
+        Me.panelModInfo.TabIndex = 4
+        '
+        'linkModSite
+        '
+        Me.linkModSite.AutoSize = True
+        Me.linkModSite.Location = New System.Drawing.Point(17, 66)
+        Me.linkModSite.Name = "linkModSite"
+        Me.linkModSite.Size = New System.Drawing.Size(144, 13)
+        Me.linkModSite.TabIndex = 2
+        Me.linkModSite.TabStop = True
+        Me.linkModSite.Text = "http://www.modwebsite.net/"
+        '
+        'labelModAuthor
+        '
+        Me.labelModAuthor.AutoSize = True
+        Me.labelModAuthor.Location = New System.Drawing.Point(17, 41)
+        Me.labelModAuthor.Name = "labelModAuthor"
+        Me.labelModAuthor.Size = New System.Drawing.Size(82, 13)
+        Me.labelModAuthor.TabIndex = 1
+        Me.labelModAuthor.Text = "ModAuthorHere"
+        '
+        'labelModname
+        '
+        Me.labelModname.AutoSize = True
+        Me.labelModname.Location = New System.Drawing.Point(17, 17)
+        Me.labelModname.Name = "labelModname"
+        Me.labelModname.Size = New System.Drawing.Size(79, 13)
+        Me.labelModname.TabIndex = 0
+        Me.labelModname.Text = "ModNameHere"
+        '
+        'buttonUninstall
+        '
+        Me.buttonUninstall.Location = New System.Drawing.Point(12, 179)
+        Me.buttonUninstall.Name = "buttonUninstall"
+        Me.buttonUninstall.Size = New System.Drawing.Size(75, 23)
+        Me.buttonUninstall.TabIndex = 5
+        Me.buttonUninstall.Text = "Remove All"
+        Me.buttonUninstall.UseVisualStyleBackColor = True
+        '
+        'panelInstall
+        '
+        Me.panelInstall.Controls.Add(Me.labelInstall)
+        Me.panelInstall.Location = New System.Drawing.Point(12, 12)
+        Me.panelInstall.Name = "panelInstall"
+        Me.panelInstall.Size = New System.Drawing.Size(293, 161)
+        Me.panelInstall.TabIndex = 6
+        '
+        'labelInstall
+        '
+        Me.labelInstall.AutoSize = True
+        Me.labelInstall.Location = New System.Drawing.Point(17, 17)
+        Me.labelInstall.Name = "labelInstall"
+        Me.labelInstall.Size = New System.Drawing.Size(48, 13)
+        Me.labelInstall.TabIndex = 0
+        Me.labelInstall.Text = "Installing"
+        '
+        'formMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 213)
+        Me.Controls.Add(Me.panelInstall)
+        Me.Controls.Add(Me.panelModInfo)
+        Me.Controls.Add(Me.panelSelectModfile)
+        Me.Controls.Add(Me.panelSelectDatfile)
+        Me.Controls.Add(Me.panelWelcome)
+        Me.Controls.Add(Me.buttonUninstall)
+        Me.Controls.Add(Me.buttonContinue)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "formMain"
+        Me.Text = "SnakeBite"
+        Me.panelWelcome.ResumeLayout(False)
+        Me.panelSelectDatfile.ResumeLayout(False)
+        Me.panelSelectDatfile.PerformLayout()
+        Me.panelSelectModfile.ResumeLayout(False)
+        Me.panelSelectModfile.PerformLayout()
+        Me.panelModInfo.ResumeLayout(False)
+        Me.panelModInfo.PerformLayout()
+        Me.panelInstall.ResumeLayout(False)
+        Me.panelInstall.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents panelWelcome As Panel
+    Friend WithEvents labelNoUninstall As Label
+    Friend WithEvents labelWelcome As Label
+    Friend WithEvents buttonContinue As Button
+    Friend WithEvents panelSelectDatfile As Panel
+    Friend WithEvents buttonBrowseMgsV As Button
+    Friend WithEvents textMGSVDir As TextBox
+    Friend WithEvents labelSelectMgsV As Label
+    Friend WithEvents panelSelectModfile As Panel
+    Friend WithEvents buttonBrowseMod As Button
+    Friend WithEvents textModFile As TextBox
+    Friend WithEvents labelSelectModfile As Label
+    Friend WithEvents panelModInfo As Panel
+    Friend WithEvents labelModname As Label
+    Friend WithEvents buttonUninstall As Button
+    Friend WithEvents linkModSite As LinkLabel
+    Friend WithEvents labelModAuthor As Label
+    Friend WithEvents panelInstall As Panel
+    Friend WithEvents labelInstall As Label
+End Class
---
+++ SnakeBite/formMain.vb
@@ -0,0 +1,305 @@
+﻿Imports ICSharpCode.SharpZipLib
+Imports System.Xml
+Imports <xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+Imports System.Threading.Thread
+Public Class formMain
+    Private Sub formMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        textMGSVDir.Text = My.Settings.MgsV
+        panelWelcome.Visible = True
+        panelSelectDatfile.Visible = False
+        panelSelectModfile.Visible = False
+        panelModInfo.Visible = False
+        panelInstall.Visible = False
+        buttonUninstall.Visible = False
+    End Sub
+
+    Private Sub buttonBrowseMgsV_Click(sender As Object, e As EventArgs) Handles buttonBrowseMgsV.Click
+        ' Browse for MGSV folder
+        Dim mgsvDirDialog As New FolderBrowserDialog
+        mgsvDirDialog.SelectedPath = textMGSVDir.Text
+        Dim dirResult = mgsvDirDialog.ShowDialog
+        If dirResult = DialogResult.OK Then
+            Dim mgsvDir = mgsvDirDialog.SelectedPath
+            ' Check for existence of MGSVTPP.EXE
+            If My.Computer.FileSystem.FileExists(mgsvDir & "\mgsvtpp.exe") Then
+                'update text box and settings
+                textMGSVDir.Text = mgsvDir
+                My.Settings.MgsV = mgsvDir
+            Else
+                MsgBox("Unable to find MGSV, please check the folder and try again.", vbOKOnly + vbCritical, "SnakeBite")
+            End If
+        End If
+    End Sub
+
+    Private Sub buttonBrowseMod_Click(sender As Object, e As EventArgs) Handles buttonBrowseMod.Click
+        ' Browse for mod file
+        Dim openModDialog As New OpenFileDialog
+        openModDialog.Filter = "MGSV Mod Files|*.mgsv|All Files|*.*"
+        Dim openResult = openModDialog.ShowDialog
+        If openResult = DialogResult.OK Then
+            textModFile.Text = openModDialog.FileName
+        End If
+    End Sub
+
+    Private Function ExtractModfile(Filename As String)
+        ' Extract the mod to a temp directory
+
+        ' Delete existing temp data
+        If My.Computer.FileSystem.FileExists("temp") Then My.Computer.FileSystem.DeleteFile("temp")
+        If My.Computer.FileSystem.DirectoryExists("_extr") Then My.Computer.FileSystem.DeleteDirectory("_extr", FileIO.DeleteDirectoryOption.DeleteAllContents)
+
+        ' Check mod file exists
+        If Not My.Computer.FileSystem.FileExists(Filename) Then
+            ' File doesn't exist, return false
+            Return False
+        End If
+
+        ' Copy file and extract
+        My.Computer.FileSystem.CopyFile(Filename, "temp")
+        Dim unzipper As New Zip.FastZip
+        Try
+            unzipper.ExtractZip("temp", "_extr", "(.*?)")
+        Catch
+            Return False
+        End Try
+        Return True
+    End Function
+
+    Private Sub buttonContinue_Click(sender As Object, e As EventArgs) Handles buttonContinue.Click
+        If panelInstall.Visible Then
+            End
+        End If
+        If panelModInfo.Visible Then
+            panelInstall.Visible = True
+            panelModInfo.Visible = False
+            buttonContinue.Visible = False
+            InstallModFile()
+
+        End If
+        If panelSelectModfile.Visible Then
+            Dim modFile = textModFile.Text
+            If Not ExtractModfile(modFile) Then
+                MsgBox("Invalid or corrupt mod file")
+                Exit Sub
+            End If
+            Dim modXml As New XmlDocument
+            Try
+                modXml.Load("_extr\mod.xml")
+            Catch
+                MsgBox("Unable to load mod metadata")
+                My.Computer.FileSystem.DeleteDirectory(modFile & "_extr", FileIO.DeleteDirectoryOption.DeleteAllContents)
+                Exit Sub
+            End Try
+            Dim modMeta = modXml.SelectSingleNode("//SnakeBite/ModData")
+            Dim modGzsData = modXml.SelectSingleNode("//GzsXmlData")
+
+            labelModname.Text = modMeta.Item("Name").InnerText
+            labelModAuthor.Text = modMeta.Item("Author").InnerText
+            linkModSite.Text = modMeta.Item("Website").InnerText
+
+            panelModInfo.Visible = True
+            panelSelectModfile.Visible = False
+            buttonUninstall.Visible = False
+        End If
+        If panelSelectDatfile.Visible Then
+            If Not My.Computer.FileSystem.FileExists(textMGSVDir.Text & "\MGSVTPP.EXE") Then
+                MsgBox("Unable to find MGSV. Check the folder or rebuild Steam data for the game.", vbOKOnly + vbCritical, "SnakeBite")
+                Exit Sub
+            End If
+            panelSelectModfile.Visible = True
+            panelSelectDatfile.Visible = False
+        End If
+        If panelWelcome.Visible Then
+            panelSelectDatfile.Visible = True
+            panelWelcome.Visible = False
+            buttonUninstall.Visible = True
+        End If
+    End Sub
+    Private Function InstallModFile()
+        Dim overFiles As Integer = 0 ' files that are likely to be overwritten
+        Dim datHashList As New List(Of String)
+        Dim datEntries As XmlNode = Nothing
+
+        Dim xmlSettings As New XmlWriterSettings
+        xmlSettings.Indent = True
+
+        Dim dat01File = My.Settings.MgsV & "\master\0\01.dat"
+        Dim dat01Dir = My.Settings.MgsV & "\master\0\01\"
+
+        If My.Computer.FileSystem.DirectoryExists(dat01Dir) Then
+            My.Computer.FileSystem.DeleteDirectory(dat01Dir, FileIO.DeleteDirectoryOption.DeleteAllContents)
+        End If
+
+        'Extract existing 01.dat
+        Dim gzsProcess As New Process
+        gzsProcess.StartInfo.FileName = "gzstool.exe"
+        gzsProcess.StartInfo.UseShellExecute = False
+        gzsProcess.StartInfo.CreateNoWindow = True
+
+        ' Load xml data
+        Dim modXml As New XmlDocument
+        modXml.Load(textModFile.Text & "_extr\mod.xml")
+        Dim modEntries = modXml.SelectSingleNode("//SnakeBite/GzsXmlData/ArchiveFile/Entries")
+
+        ' Load mod hashes
+        Dim modHashList As New List(Of String)
+        For Each moe As XmlNode In modEntries.ChildNodes
+            modHashList.Add(moe.Attributes("Hash").Value)
+        Next
+
+        If My.Computer.FileSystem.FileExists(dat01File) Then
+            ' Extract 01.dat
+            gzsProcess.StartInfo.Arguments = dat01File
+            gzsProcess.Start()
+
+            labelInstall.Text = "Unpacking game data..."
+            Do Until gzsProcess.HasExited
+                Application.DoEvents()
+            Loop
+
+            ' Open 01.dat.xml
+            Dim datXml As New XmlDocument
+            datXml.Load(dat01File & ".xml")
+            datEntries = datXml.SelectSingleNode("//ArchiveFile/Entries")
+
+            ' Load dat hashes
+            For Each moe As XmlNode In datEntries.ChildNodes
+                datHashList.Add(moe.Attributes("Hash").Value)
+            Next
+
+            'Compare extracted hashes to mod hashes
+            For Each mhe In modHashList
+                If datHashList.Contains(mhe) Then
+                    overFiles += 1
+                End If
+            Next
+
+            My.Computer.FileSystem.DeleteFile(dat01File & ".xml")
+        End If
+
+        'If any files matched , warn user they may be incompatible
+        If overFiles > 0 Then
+            Dim overFile = MsgBox("Some files in the mod you have selected may overwrite files for an existing mod. Click OK to install anyway, or cancel to exit SnakeBite.", vbOKCancel + vbExclamation, "SnakeBite")
+            If overFile = MsgBoxResult.Cancel Then
+                End
+            End If
+        End If
+
+        ' Check for fpk files inside mod archive
+        Dim fpksToMerge As New List(Of String)
+        For Each modEntry As XmlNode In modEntries.ChildNodes
+            Dim filePath As String = modEntry.Attributes("FilePath").Value
+            Dim fileType = filePath.Substring(filePath.LastIndexOf(".") + 1).ToLower
+            Dim fileHash = modEntry.Attributes("Hash").Value
+            If fileType = "fpk" Or fileType = "fpkd" Then
+                ' If the FPK already existed, it will be merged
+                If datHashList.Contains(fileHash) Then
+                    fpksToMerge.Add(filePath)
+                End If
+            End If
+        Next
+
+        'Extract fpk files
+        labelInstall.Text = "Unpacking mod data..."
+        For Each fpkfile In fpksToMerge
+            gzsProcess.StartInfo.Arguments = dat01Dir & fpkfile
+            gzsProcess.Start()
+            gzsProcess.StartInfo.Arguments = "_extr\" & fpkfile
+            gzsProcess.Start()
+            Do Until gzsProcess.HasExited
+                Application.DoEvents()
+            Loop
+        Next
+
+        'copy files
+        labelInstall.Text = "Applying mod changes..."
+        My.Computer.FileSystem.CopyDirectory("_extr", dat01Dir, True)
+
+        'rebuild fpk xml
+        labelInstall.Text = "Rebuilding mod data..."
+        For Each fpkfile In fpksToMerge
+            Dim fpkdir As String = dat01Dir & fpkfile.Replace(".", "_")
+            Dim filetype = fpkfile.Substring(fpkfile.LastIndexOf(".") + 1).ToLower
+            Dim fpkfilename = fpkfile.Substring(fpkfile.LastIndexOf("\") + 1).ToLower
+            Dim fpkxmlw As XmlWriter = XmlWriter.Create(dat01Dir & fpkfile & ".xml")
+
+            fpkxmlw.WriteStartDocument()
+            fpkxmlw.WriteStartElement("ArchiveFile")
+
+            fpkxmlw.WriteAttributeString("Name", fpkfilename)
+            fpkxmlw.WriteAttributeString("FpkType", filetype)
+
+            fpkxmlw.WriteStartElement("Entries")
+
+            Dim dirfiles = My.Computer.FileSystem.GetDirectoryInfo(fpkdir).GetFiles("*.*", IO.SearchOption.AllDirectories)
+            For Each f In dirfiles
+                fpkxmlw.WriteStartElement("Entry")
+                Dim fpath = f.FullName
+                Dim endpath = fpath.Substring(fpath.LastIndexOf("\Assets"))
+                endpath = endpath.Replace("\", "/")
+                fpkxmlw.WriteAttributeString("FilePath", endpath)
+                fpkxmlw.WriteEndElement()
+            Next
+
+            fpkxmlw.WriteEndElement()
+            fpkxmlw.WriteEndElement()
+
+            fpkxmlw.WriteEndDocument()
+
+            fpkxmlw.Close()
+            fpkxmlw.Dispose()
+
+            gzsProcess.StartInfo.Arguments = fpkfile & ".xml"
+            gzsProcess.Start()
+            Do Until gzsProcess.HasExited
+                Application.DoEvents()
+            Loop
+        Next
+
+        'rebuild 01.dat xml
+        labelInstall.Text = "Rebuilding game archive..."
+        Dim datXmlWriter As XmlWriter = XmlWriter.Create(dat01File & ".xml", xmlSettings)
+        datXmlWriter.WriteStartDocument()
+        datXmlWriter.WriteStartElement("ArchiveFile")
+        datXmlWriter.WriteAttributeString("xsi", "type", "http://www.w3.org/2001/XMLSchema-instance", "QarFile")
+        datXmlWriter.WriteAttributeString("Name", "01.dat")
+        datXmlWriter.WriteAttributeString("Flags", "3150048")
+
+        datXmlWriter.WriteStartElement("Entries")
+        If datEntries IsNot Nothing Then
+            For Each datEntry As XmlNode In datEntries.ChildNodes
+                datXmlWriter.WriteRaw(datEntry.OuterXml)
+            Next
+        End If
+
+        For Each modEntry As XmlNode In modEntries.ChildNodes
+            If Not datHashList.Contains(modEntry.Attributes("Hash").Value) Then
+                datXmlWriter.WriteRaw(modEntry.OuterXml & vbNewLine)
+            End If
+        Next
+        datXmlWriter.WriteEndElement()
+        datXmlWriter.WriteEndElement()
+        datXmlWriter.WriteEndDocument()
+        datXmlWriter.Close()
+        datXmlWriter.Dispose()
+
+        'repack 01.dat
+        gzsProcess.StartInfo.Arguments = dat01File & ".xml"
+        gzsProcess.Start()
+
+        Do Until gzsProcess.HasExited
+            Application.DoEvents()
+        Loop
+
+        labelInstall.Text = "Installation complete"
+        buttonContinue.Text = "Close"
+        buttonContinue.Visible = True
+    End Function
+
+    Private Sub buttonUninstall_Click(sender As Object, e As EventArgs) Handles buttonUninstall.Click
+        Dim youSure = MsgBox("This will remove all SnakeBite mods, are you sure?", vbYesNo + vbQuestion, "SnakeBite")
+        If youSure = MsgBoxResult.Yes Then
+
+        End If
+    End Sub
+End Class
